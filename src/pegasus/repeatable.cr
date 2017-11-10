ee23bfe7ccd2d03db6adb5a840a44d179b0eda3b
Pawan Dubey <git@pawandubey.com>
2017-11-10 00:51:17 -0500

    Raise if num supplied to repeatable is negative

--- src/pegasus/repeatable.cr
+++ src/pegasus/repeatable.cr
@@ -3,11 +3,23 @@ module Pegasus
     alias Number = Int32 | UInt64
 
     def repeat(num : Number)
+      raise ArgumentError.new("Expected positive. Was #{num}.") if num < 0
+
       Pegasus::Extensions::Repetition.new(self, num, num)
     end
 
     def repeat(min = 0, max = UInt64::MAX)
+      [min, max].each do |num|
+        raise ArgumentError.new("Expected positive. Was #{num}.") if num < 0
+      end
+
       Pegasus::Extensions::Repetition.new(self, min, max)
+    end
+
+    def at_least(num : Number)
+      raise ArgumentError.new("Expected positive. Was #{num}.") if num < 0
+
+      Pegasus::Extensions::Repetition.new(self, num, UInt64::MAX)
     end
 
     def maybe?
